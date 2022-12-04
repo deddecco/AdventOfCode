f33c1bb6cfa9e5d26c5afa13c13e834fe7bc4fbf
Andre da Silva <apdasil@emory.edu>
2022-12-04 16:57:37 -0500

    renamed class

--- src/main/java/ElfSectionOverlapPairsCounter.java
+++ src/main/java/ElfSectionOverlapPairsCounter.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ElfSectionPairsCounter {
+public class ElfSectionOverlapPairsCounter {
 
      public static void main(String[] args) throws IOException {
           String filePath = "C:\\Users\\andre\\IdeaProjects\\AdventOfCode\\elf-input.txt";
